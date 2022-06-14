c81ee86edfec40353517be7850407c9cc99752f3
MineCodeBr <93443182+MineCodeBr@users.noreply.github.com>
2022-06-14 08:09:37 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 try {
-exports.Manifest = require('./main/Manifest')
-exports.ItemAutomatic = require('./main/ItemAuto')
-exports.TemplateAddon = require('./main/TemplateAddon')
-exports.Formater = require('./main/Formater')
+exports.Manifest = require('./main/Manifest.js')
+exports.ItemAutomatic = require('./main/ItemAuto.js')
+exports.TemplateAddon = require('./main/TemplateAddon.js')
+exports.Formater = require('./main/Formater.js')
 } catch (e) {
  console.error(`[ERRO MCBE] ` + e)
 }
